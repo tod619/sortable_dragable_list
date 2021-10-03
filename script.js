68d2c841d62b763e21120c4435b4817872260226
tod619 <trevorodwyer619@hotmail.com>
2021-10-03 19:30:57 +0100

    add items to list using js in script.js file

---
+++ script.js
@@ -0,0 +1,44 @@
+const draggable_list = document.getElementById('draggable-list')
+const checkBtn = document.getElementById('check')
+
+const richestPeople = [
+    'Jeff Bezor',
+    'Bill Gates',
+    'Warren Buffett',
+    'Bernard Arnault',
+    'Carlos Slim Helu',
+    'Amancio Ortega',
+    'Larry Ellison',
+    'Mark Zuckerberg',
+    'Michael Bloomberg',
+    'Larry Page'    
+    ]
+
+// Store List Itmes
+const listItems = []
+
+let dragStartIndex
+
+// Insert List Items Into Dom
+function createList() {
+    [...richestPeople].forEach((person,idx) => {
+        const listItem = document.createElement('li')
+
+        listItem.setAttribute('data-index',idx)
+
+        listItem.innerHTML = `
+        <span class="number">${idx + 1}</span>
+        <div class = "draggable" draggable = "true">
+            <p class = "person-name">${person}</p>
+            <i class = "fas fa-grip-lines"></i>
+        </div>
+        `
+
+        listItems.push(listItem)
+
+        draggable_list.appendChild(listItem)
+    })
+}
+
+createList()
+
